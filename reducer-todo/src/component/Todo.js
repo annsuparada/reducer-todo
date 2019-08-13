0bf4e2ea91111664b3dc469fd8190bb56c948e1e
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-13 15:45:31 -0500

    working on step 3

--- reducer-todo/src/component/Todo.js
+++ reducer-todo/src/component/Todo.js
@@ -11,7 +11,7 @@ const Todo = () => {
             {state.map(todo => {
                 return <h1 key={todo.id}>{todo.item}</h1>
             })}
-            
+            <input />
         </div>
     )
 }
